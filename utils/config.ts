3d1faeefc836956271f4c372c2206a7ac0dd35f6
THE-DEEPDAS <deepdblm@gmail.com>
2024-12-26 21:30:00 +0530

    lot of tries

---
+++ utils/config.ts
@@ -0,0 +1,22 @@
+export const getBaseUrl = (): string => {
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+  return 'http://localhost:3000';
+};
+
+export type Routes = {
+  home: string;
+  results: string;
+  api: {
+    getAdvice: string;
+  };
+};
+
+export const routes: Routes = {
+  home: '/',
+  results: '/results',
+  api: {
+    getAdvice: '/api/getAdvice',
+  },
+};
